7b7af069e82f7283bf073e6989344a27bacedfc2
wanghongzheng <wanghongzheng@712.cn>
2020-06-11 14:09:10 +0800

    提交状态管理

--- src/components/increament.vue
+++ src/components/increament.vue
@@ -1,12 +1,17 @@
 <template>
-<div>
-    <div>
-        <button @click="inscreament">+1</button>
-        <button @click="decreament">-1</button>
+<div >
+    <div v-if ="show" class="loading">
+        数据提交中。。。。
     </div>
-    <div>
-        <input type="text" v-model="incrementValue" class="input">
-        <button @click="incrementWithValue" class="addButton">增加</button>
+    <div v-else>
+        <div>
+            <button @click="inscreament">+1</button>
+            <button @click="decreament">-1</button>
+        </div>
+        <div>
+            <input type="text" v-model="incrementValue" class="input">
+            <button @click="incrementWithValue" class="addButton">增加</button>
+        </div>
     </div>
 </div>
 </template>
@@ -16,6 +21,11 @@ export default {
     data(){
         return {
             incrementValue: 0
+        }
+    },
+    computed: {
+        show: function() {
+            return this.$store.state.waiting;
         }
     },
     methods:{
@@ -31,15 +41,19 @@ export default {
         margin: 10px;
     }
     button {
-        width:100px;
-        height:100px;
+        width: 100px;
+        height: 100px;
     }
     .addButton {
-        width:80px;
-        height:60px;
+        width: 80px;
+        height: 60px;
     }
     .input {
-        height:50px;
+        height: 50px;
         line-height: 50px;
+    }
+    .loading{
+        background: rgba(0,0,0,0.3);
+        height: 100%;
     }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,17 +5,28 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        count: 20
+        count: 20,
+        waiting: false
     },
     mutations: {
+        // 加1
         INSCREAMENT(state){
             state.count ++ ;
         },
+        // 减1
         DECREMENT(state){
             state.count --;
         },
+        // 加输入值
         INCREMENT_WITH_VALUE(state, value){
             state.count += value;
+        },
+        // 显示和隐藏waiting
+        SHOW_WAITING_MESSAGE(state){
+            state.waiting = true;
+        },
+        HIDE_WAITNG_MESSAGE(state){
+            state.waiting = false;
         }
     },
     actions:{
@@ -25,8 +36,21 @@ const store = new Vuex.Store({
         decreament({commit}){
             commit("DECREMENT");
         },
+        //提交
         incrementWithValue({commit},value){
-            commit("INCREMENT_WITH_VALUE",parseInt(value));
+            //显示loading
+            commit('SHOW_WAITING_MESSAGE');
+            //异常处理
+            setTimeout(function(){
+                if(isNaN(value)){
+                    alert("Not an Interger");
+                }else {
+                    //loading隐藏
+                    commit("HIDE_WAITNG_MESSAGE");
+                    //提交数据
+                    commit("INCREMENT_WITH_VALUE",parseInt(value));
+                }
+            },2000);
         }
     }
 })
